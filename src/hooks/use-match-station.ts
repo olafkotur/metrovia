62895df33243561e3d137673aca841d9f1313a54
olafkotur <olafkotur97@gmail.com>
2024-01-19 11:53:01 +0000

    Improved matching aglorithm

--- src/hooks/use-match-station.ts
+++ src/hooks/use-match-station.ts
@@ -1,12 +1,23 @@
 import { useCallback } from 'react';
 import { Station } from '../typings';
 
+const MATCH_REGEX = /[^a-z0-9]/g;
+const BLACKLISTED_WORDS = ['and'];
+
 export const useMatchStation = () => {
   return useCallback((value: string, stations: Station[]) => {
     const filtered = stations.filter((station) => station.visible === false);
     const station = filtered.find((station) => {
-      const stationFormatted = station.name.toLowerCase().replace(/[^a-z0-9]/g, '');
-      const valueFormatted = value.toLowerCase().replace(/[^a-z0-9]/g, '');
+      let stationFormatted = station.name.toLowerCase().replace(MATCH_REGEX, '');
+      let valueFormatted = value.toLowerCase().replace(MATCH_REGEX, '');
+
+      // remove blacklisted words from matching
+      for (const word of BLACKLISTED_WORDS) {
+        const regex = new RegExp(word, 'g');
+        stationFormatted = stationFormatted.replace(regex, '');
+        valueFormatted = valueFormatted.replace(regex, '');
+      }
+
       return stationFormatted === valueFormatted;
     });
 
